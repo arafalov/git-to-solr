67d84d29bd6c2e820781cf44ebe4fa6161cdbf17
Alexandre Rafalovitch <arafalov@gmail.com>
2014-06-28 21:42:25 +0700

    Add actual Solr query running

--- ui/js/solr.js
+++ ui/js/solr.js
@@ -1,14 +1,27 @@
 var solrApp = angular.module('solrApp', []);
 
-solrApp.controller('SolrCtrl', function($scope) {
+solrApp.controller('SolrCtrl', function($scope, $http) {
     $scope.handlerUrl = "http://localhost:8983/solr/select";
 
     $scope.queries = [
-    {name: 'Get first 10', definition: 'q=*:*'},
-    {name: 'Some other query', definition: 'q=*:*&fq=foobar'}
+    {name: 'Commits with children', definition: 'q=type:commit&fl=*,[child parentFilter=type:commit]'},
+    {name: 'Commits only', definition: 'q=*:*&fq=type:commit'}
     ];
 
     $scope.runQuery = function(query){
         $scope.activeQuery = $scope.handlerUrl + "?" + query;
+        $scope.status = null;
+
+        console.log('http');
+        $http.get($scope.activeQuery + '&wt=json')
+        .success(function(data){
+            $scope.status = 'Succcess';
+            $scope.docs = data.response.docs;
+            $scope.docsStr = JSON.stringify($scope.docs, null, '  ');
+        })
+        .error(function(status, data) {
+            $scope.status = 'Error';
+            $scope.docs = $scope.docsStr = null;
+        });
     }
 })
